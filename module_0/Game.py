0f66f7207688f03ed10ebb6cdad9020d60d40339
Burn10110 <burnusini@gmail.com>
2020-10-27 12:36:08 +0300

    Change name of variable

--- module_0/Game.py
+++ module_0/Game.py
@@ -2,16 +2,16 @@ import numpy as np
 limit = int(input('В каких пределах угадываем? От 1 до ')) #мне стало интересно, как будет меняться число попыток от верхнего предела выборки
 #limit = 100 #если нужно именно 100 в решении
 def game_core():
-    predict = np.random.randint(1,limit+1) #загадываем число
+    number = np.random.randint(1,limit+1) #загадываем число
     compare = limit #ищущее число, изначально равно нашему диапазону
     interval = limit #вспомогательное число стремящееся к нулю делением на 2, изначально равно ищущему
     count = 0 #счётчик попыток
     for i in range(1, limit+1):
         count += 1 #считаем попытку
-        if predict==compare:
+        if number==compare:
             return(count)
             break #если нашли - отбой
-        elif predict<compare: #если искомое меньше
+        elif number<compare: #если искомое меньше
             interval //= 2 #делим вспомогательное на два
             compare -= interval #и отнимаем от ищущего числа
             if interval<1: #исправляем ошибку округления на финальном шаге
